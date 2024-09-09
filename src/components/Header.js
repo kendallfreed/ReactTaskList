46599045ec12b8eaed88405dcafc4e3665b43554
Kendall Freed <kfre157@wgu.edu>
2024-09-09 09:46:12 -0700

    Task List using React, CSS, JavaScript

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+import Logo from "../assets/rabbit.png"
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <a href="/">Home</a>
+      <img className = "logo" src = {Logo} alt = "" />
+    </header>
+  )
+}
